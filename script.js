9cc2f49c169bc4884d229d429955706bb66948cd
korean-0811 <snow1pass@proton.me>
2025-04-10 10:11:16 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function showSection(id) {
-  document.querySelectorAll('.section').forEach(el => el.classList.add('hidden'));
-  document.getElementById(id).classList.remove('hidden');
-}
-function toggleLogin() {
-  const popup = document.getElementById('loginPopup');
-  popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
-}
